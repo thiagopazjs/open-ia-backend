d579dd3fd5d8075e199175ac5d16824153b59cef
Thiago Paz <tpazdev@gmail.com>
2023-09-14 11:25:01 -0300

    feat: add OpenAI and transcription route

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -41,9 +41,18 @@ export async function uploadVideoRoute(app:FastifyInstance) {
     const uploadDestination = path.resolve(__dirname, '../../tmp',fileUploadName)
 
     await pump(data.file, fs.createWriteStream(uploadDestination))
+   
 
-    return reply.send()
+    const video = await prisma.video.create ({
+        data: {
+            name: data?.filename,
+            path: uploadDestination,
+        }
+    })
 
+    return {
+        video,
+    }
 
     })
     
