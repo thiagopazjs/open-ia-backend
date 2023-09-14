9e45fd89d11f1a6e93b8fb05d4ffdf2ccd4b586f
Thiago Paz <tpazdev@gmail.com>
2023-09-14 09:59:34 -0300

    feat: add upload route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import {fastify} from'fastify';
 import { prisma } from './lib/prisma';
 import { getAllPrompts } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
 
 const app = fastify()
 
 app.register(getAllPrompts)
+app.register(uploadVideoRoute)
 
 app.listen ({
     port:3333,
